2a55f4fab932872510423677429afc5231912e84
henry <henryonyemaobi@gmail.com>
2020-01-09 00:05:08 +0100

    set up api routes for subscribers and fields

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/subscribers', 'SubscribersController');
+Route::apiResource('/fields', 'FieldsController');
