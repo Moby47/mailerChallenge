2d65f0b5adf50a665b5ab75662a2acb7e16f5b72
Moby47 <henryonyemaobi@gmail.com>
2020-01-08 08:17:09 +0000

    Commit to establish a one to many relationship between subscribers and fields

--- app/Fields.php
+++ app/Fields.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fields extends Model
 {
-    //
+     /**
+     * Get the subscribers that owns the field.
+     */
+    public function subscribers()
+    {
+        return $this->belongsTo('App\Subscribers');
+    }
 }
--- app/Subscribers.php
+++ app/Subscribers.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscribers extends Model
 {
-    //
+    /**
+     * Get the fields for the subscriber.
+     */
+    public function fields()
+    {
+        return $this->hasMany('App\Fields');
+    }
 }
