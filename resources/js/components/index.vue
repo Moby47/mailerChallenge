995f4e7f3e4e2bab74194dd98b15e39b757da8cd
henry <henryonyemaobi@gmail.com>
2020-01-09 23:50:18 +0100

    creates UI design for landing and subscribers pages

--- resources/js/components/index.vue
+++ resources/js/components/index.vue
@@ -1,51 +1,123 @@
 <template>
-  <v-card
-    class="mx-auto"
-    height="600"
-    width="900"
-  >
-    <v-navigation-drawer
-      absolute
-      dark
-      src="/images/home.png"
-      width="100%"
-      permanent
+    <v-card
+      class="mx-auto"
+      height="600"
+      width="900"
     >
-      <v-list>
-        <v-list-item
-          v-for="([link, text], i) in items"
-          :key="i"
-          link
-        >
-          <v-list-item-content>
-            <v-list-item-title><b ><v-btn color="success" ><router-link :to='link' class='text-style'>{{ text }}</router-link></v-btn></b></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+        <div>
+                <b-navbar toggleable="sm" type="light" variant="light">
+                  <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+                 
+                  <b-collapse id="nav-text-collapse" is-nav>
+                    <b-navbar-nav>
+                      <b-nav-text><router-link to='/subscribe'>Subscribe</router-link></b-nav-text>
+                    </b-navbar-nav>
+                  </b-collapse>
+
+                  <b-collapse id="nav-text-collapse" is-nav>
+                      <b-navbar-nav>
+                        <b-nav-text><router-link to='/subscribers'>All Subscribers</router-link></b-nav-text>
+                      </b-navbar-nav>
+                    </b-collapse>
+                </b-navbar>
+              </div>
+
+
+
+              <template>
+                  <div>
+                    <b-carousel
+                      id="carousel-1"
+                      v-model="slide"
+                      :interval="2000"
+                      controls
+                      indicators
+                      background="#ababab"
+                      img-width="1024"
+                      img-height="440"
+                      style="text-shadow: 1px 1px 2px #333;"
+                      @sliding-start="onSlideStart"
+                      @sliding-end="onSlideEnd"
+                    >
+                      <!-- Text slides with image -->
+                      <b-carousel-slide
+                        text="Supercharge your email marketing today."
+                        img-src="/images/home.png"
+                      ></b-carousel-slide>
+                
+                      <!-- Text slides with image -->
+                      <b-carousel-slide
+                        text="Supercharge your email marketing today."
+                        img-src="/images/home.png"
+                      ></b-carousel-slide>
+                
+                      <!-- Text slides with image -->
+                      <b-carousel-slide
+                        text="Supercharge your email marketing today."
+                        img-src="/images/home.png"
+                      ></b-carousel-slide>
+
+                    </b-carousel>
+                  </div>
+                </template>
+
+
+                <div>
+                    <b-jumbotron>
+                      <template v-slot:header>Welcome</template>
+                  
+                      <template v-slot:lead>
+                          Our support team works 24/7 to assist you . Reach out to us and we will get back to you.
+                      </template>
+                  
+                      <hr class="my-4">
+                  
+                      <p>
+                          Join our happy subscriberss worldwide.
+                      </p>
+                  
+                      <b-button variant="primary" ><router-link to='/subscribe' class='text-style'>Join Now</router-link></b-button>
+                      <b-button variant="success" ><router-link to='/subscribers' class='text-style'>Our Subscribers</router-link></b-button>
+                    </b-jumbotron>
+                  </div>
+
+
+                  
+                      <v-footer padless>
+                        <v-col
+                          class="text-center"
+                          cols="12"
+                        >
+                          {{ new Date().getFullYear() }} — <strong>Henry Onyemaobi</strong>
+                        </v-col>
+                      </v-footer>
+</v-card>
 </template>
+
 
 
 <script>
 
-export default {
-
-data: () => ({
-  items: [
-        ['/subscribe', 'Subscribe'],
-        ['/subscribers', 'Our Subscribers'],
-      ]     
-}),
-  
-methods: {
-
-        
-},
-
-mounted() {
-
-}
-
-}
-</script>
+    export default {
+    
+    data: () => ({
+      slide: 0,
+        sliding: null
+    }),
+      
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+            
+    },
+    
+    mounted() {
+    
+    }
+    
+    }
+    </script>
---
+++ resources/js/components/subscribers.vue
@@ -0,0 +1,150 @@
+<template>
+        <v-card
+          class="mx-auto"
+          height="600"
+          width="900"
+        >
+            <div>
+                    <b-navbar toggleable="sm" type="light" variant="light">
+                      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+                      <b-collapse id="nav-text-collapse" is-nav>
+                            <b-navbar-nav>
+                              <b-nav-text><router-link to='/'>Home</router-link></b-nav-text>
+                            </b-navbar-nav>
+                          </b-collapse>
+                      <b-collapse id="nav-text-collapse" is-nav>
+                        <b-navbar-nav>
+                          <b-nav-text><router-link to='/subscribe'>Subscribe</router-link></b-nav-text>
+                        </b-navbar-nav>
+                      </b-collapse>
+                    </b-navbar>
+                  </div>
+
+
+
+                  <template>
+                        <v-card >
+                            <v-container>
+                            <v-row dense>
+
+
+
+                              <v-col cols="6">
+                                <v-card
+                                  color="#385F73"
+                                  dark
+                                >
+                              <v-card-title class="headline">First name surname</v-card-title>
+                      
+                               <v-card-subtitle>email@gmail.com</v-card-subtitle>
+                               <v-card-subtitle>State</v-card-subtitle>
+                      
+                                  <v-card-actions>
+                                    <v-btn >My Fields</v-btn>
+                                  </v-card-actions>
+                                </v-card>
+                              </v-col>
+                      
+
+
+                              <v-col cols="6">
+                                    <v-card
+                                      color="#385F73"
+                                      dark
+                                    >
+                                  <v-card-title class="headline">First name surname</v-card-title>
+                          
+                                   <v-card-subtitle>email@gmail.com</v-card-subtitle>
+                                   <v-card-subtitle>State</v-card-subtitle>
+                          
+                                      <v-card-actions>
+                                        <v-btn >My Fields</v-btn>
+                                      </v-card-actions>
+                                    </v-card>
+                                  </v-col>
+                                
+
+                                  <v-col cols="6">
+                                        <v-card
+                                          color="#385F73"
+                                          dark
+                                        >
+                                      <v-card-title class="headline">First name surname</v-card-title>
+                              
+                                       <v-card-subtitle>email@gmail.com</v-card-subtitle>
+                                       <v-card-subtitle>State</v-card-subtitle>
+                              
+                                          <v-card-actions>
+                                            <v-btn >My Fields</v-btn>
+                                          </v-card-actions>
+                                        </v-card>
+                                      </v-col>
+
+                                      <v-col cols="6">
+                                            <v-card
+                                              color="#385F73"
+                                              dark
+                                            >
+                                          <v-card-title class="headline">First name surname</v-card-title>
+                                  
+                                           <v-card-subtitle>email@gmail.com</v-card-subtitle>
+                                           <v-card-subtitle>State</v-card-subtitle>
+                                  
+                                              <v-card-actions>
+                                                <v-btn >My Fields</v-btn>
+                                              </v-card-actions>
+                                            </v-card>
+                                          </v-col>
+
+
+                                </v-row>
+                          </v-container>
+                        </v-card>
+
+
+                        <div class='text-center mt-4 mb-2'>
+                                <b-button pill variant="outline-secondary">PREV</b-button>
+                                <b-button pill variant="outline-secondary">NEXT</b-button>
+                              </div>
+
+
+                      </template>
+
+                      <v-footer padless>
+                            <v-col
+                              class="text-center"
+                              cols="12"
+                            >
+                              {{ new Date().getFullYear() }} — <strong>Henry Onyemaobi</strong>
+                            </v-col>
+                          </v-footer>
+
+
+
+    </v-card>
+</template>
+
+
+
+<script>
+
+        export default {
+        
+        data: () => ({
+          items: [
+                ['/subscribe', 'Subscribe'],
+                ['/subscribers', 'Our Subscribers'],
+              ]     
+        }),
+          
+        methods: {
+        
+                
+        },
+        
+        mounted() {
+        
+        }
+        
+        }
+        </script>
